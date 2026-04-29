b7967f11802394896cb8851f4d2c6adab0ee9a66
meanie <nqminh146@gmail.com>
2026-04-29 19:09:27 +0200

    cleanup split_nodes_image.py

--- src/split_nodes_image.py
+++ src/split_nodes_image.py
@@ -32,30 +32,3 @@ def split_nodes_image(old_nodes: list[TextNode]):
         if previous_end < len(string):
             new_nodes.append(TextNode(string[previous_end:], TextType.PLAIN))
     return new_nodes
-
-
-if __name__ == '__main__':
-    pattern = r'!\[(?P<image_text>[^\(]*)\]\((?P<image_url>[^\[]*)\)'
-    string = 'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and ![another image](https://www.boot.dev/lessons/21db95df-68e9-4f10-9c76-16142abba580) and a [link](https://boot.dev)'
-    
-    '''
-    result = []
-    previous_end = 0
-
-    for m in re.finditer(pattern, string):
-        if m.start() > previous_end:
-            result.append(TextNode(string[previous_end:m.start()], TextType.PLAIN))
-            # result.append({"type": "text", "content": string[previous_end:m.start()]})
-        result.append(TextNode(m.group('image_text'), TextType.IMAGE, m.group('image_url')))
-        # result.append({"type": "image", **m.groupdict()})
-        previous_end = m.end()
-    
-    if previous_end < len(string):
-        result.append(TextNode(string[previous_end:], TextType.PLAIN))
-        # result.append({"type": "text", "content": string[previous_end:]})
-    '''
-
-    node = TextNode(string, TextType.PLAIN)
-    result = split_nodes_image([node])
-    for item in result:
-        print(item)
